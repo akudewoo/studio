c56319b89fbf12051fc9c33a0a63c77e6e4b8006
Wanaji Widuro Dewo <dewoapps@gmail.com>
2025-08-25 19:49:06 +0000

    rubah iconnya dengan initial TM

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -3,20 +3,21 @@ export function Logo(props: React.SVGProps<SVGSVGElement>) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      fill="currentColor"
       {...props}
     >
-      <path d="M21 10V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v2" />
-      <path d="M21 14v-4" />
-      <path d="M3 14v-4" />
-      <path d="M12 18.5V14" />
-      <path d="m21 14-9 4.5-9-4.5" />
-      <path d="M3.27 10 12 14.5l8.73-4.5" />
-      <path d="M12 22.5 3 17.25V14l9 4.5 9-4.5v3.25L12 22.5z" />
+      <text
+        x="50%"
+        y="50%"
+        dominantBaseline="middle"
+        textAnchor="middle"
+        fontSize="12"
+        fontWeight="bold"
+        fill="currentColor"
+        fontFamily="sans-serif"
+      >
+        TM
+      </text>
     </svg>
   );
 }
