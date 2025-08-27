a46c77de53cf33e899f2049a7a2bf73f2188d6c6
Wanaji Widuro Dewo <dewoapps@gmail.com>
2025-08-27 08:30:04 +0000

    firestore database saya aja gada ??

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,14 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration, directly embedded
 const firebaseConfig = {
-  apiKey: "AIzaSyC5Nz9l6wcUs5c-HqsrhR6kXJmar-dX0j4",
-  authDomain: "alur-distribusi.firebaseapp.com",
-  projectId: "alur-distribusi",
-  storageBucket: "alur-distribusi.firebasestorage.app",
-  messagingSenderId: "844802255380",
-  appId: "1:844802255380:web:5485d5ed43717bd6c29501"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 
